c9187b77c2edc9b06d0914df6d09e8624d4f7600
mateuszzinn <mateuslima2002.ml@gmail.com>
2024-09-08 00:08:10 -0300

    Finalizando gerente

--- persistencia/src/main/java/org/example/Main.java
+++ persistencia/src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import org.example.controller.GerenteController;
-import org.example.model.Gerente;
+import org.example.model.*;
 import org.example.utils.paths.Path;
 
 import java.io.File;
@@ -13,7 +13,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         GerenteController controller = new GerenteController();
-        //controller.cadastrarGerente(new Gerente("Mateus", "mateuslima74", "123", "mateus@gmail.com", 1231));
-        System.out.println(controller.fazerLogin(new Gerente("Mateus", "mateuslima74", "123", "mateus@gmail.com", 1231)));
+        //controller.cadastrarGerente(new Gerente("George", "mateuslima74", "123", "mateus@gmail.com", 1231));
+        //System.out.println(controller.fazerLogin(new Gerente("Mateus", "mateuslima74", "123", "mateus@gmail.com", 1231)));
+        //controller.cadastrarAlmoxerife(new Almoxerife("George", "mateuslima74", "123", "mateus@gmail.com", 1231));
+        //controller.cadastrarCaixaEletronico(new CaixaEletronico("George O BRABO", "mateuslima74", "123", "mateus@gmail.com", 1231));
+        //controller.cadastrarProduto(new Produto(5, "Arroz", 100, 4.50, 5.80));
+        //System.out.println(controller.exibirDetalhesProduto(5));
+        //controller.editarProduto(5, new Produto(15, "Feijas", 12, 5.50, 6.80));
+
+        controller.cadastrarCliente(new Cliente("George", 123123321, "george@gmail.com", "Coxixola"));
     }
 }
--- persistencia/src/main/java/org/example/controller/GerenteController.java
+++ persistencia/src/main/java/org/example/controller/GerenteController.java
@@ -3,8 +3,7 @@ package org.example.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.model.Gerente;
-import org.example.model.Produto;
+import org.example.model.*;
 import org.example.utils.paths.Path;
 
 import java.io.File;
@@ -19,7 +18,7 @@ public class GerenteController {
     // 1- Cadastro de Gerente
     public void cadastrarGerente(Gerente gerente) throws IOException {
         criarArquivo(Path.GERENTE_PATH);
-        List<Gerente> gerentes = lerArquivoGerente(Path.GERENTE_PATH);
+        List<Gerente> gerentes = lerArquivoGerente();
 
         if (gerentes == null) {
             gerentes = new ArrayList<>();
@@ -31,13 +30,13 @@ public class GerenteController {
 
     // 2- Fazer login
     public boolean fazerLogin(Gerente gerente) throws IOException {
-        List<Gerente> gerentes = lerArquivoGerente(Path.GERENTE_PATH);
+        List<Gerente> lista = (List<Gerente>) lerArquivoGerente();
 
-        if (gerentes == null) {
-            gerentes = new ArrayList<>();
+        if (lista == null) {
+            lista = new ArrayList<>();
         }
 
-        for(Gerente g : gerentes){
+        for(Gerente g : lista){
             if(g.getLogin().equals(gerente.getLogin()) && g.getSenha().equals(gerente.getSenha())){
                 return true;
             }
@@ -45,13 +44,15 @@ public class GerenteController {
         return false;
     }
 
-    /*
+
     // 3- Cadastro de almoxerife
     public void cadastrarAlmoxerife(Almoxerife almoxerife){
         criarArquivo(Path.ALMOXERIFE_PATH);
 
-        List<Almoxerife> lista = (List<Almoxerife>) lerArquivo(Path.ALMOXERIFE_PATH);
-
+        List<Almoxerife> lista = lerArquivoAlmoxerife();
+        if(lista == null){
+            lista = new ArrayList<>();
+        }
         lista.add(almoxerife);
         escreverNoArquivo(Path.ALMOXERIFE_PATH, lista);
     }
@@ -60,7 +61,10 @@ public class GerenteController {
     public void cadastrarCaixaEletronico(CaixaEletronico caixa){
         criarArquivo(Path.CAIXA_PATH);
 
-        List<CaixaEletronico> lista = (List<CaixaEletronico>) lerArquivo(Path.CAIXA_PATH);
+        List<CaixaEletronico> lista = (List<CaixaEletronico>) lerArquivoCaixaEletronico();
+        if(lista == null){
+            lista = new ArrayList<>();
+        }
 
         lista.add(caixa);
         escreverNoArquivo(Path.CAIXA_PATH, lista);
@@ -69,7 +73,7 @@ public class GerenteController {
     // 4- Cadastro de produtos
     public void cadastrarProduto(Produto produto) {
         criarArquivo(Path.PRODUTOS_PATH);
-        List<Produto> lista = (List<Produto>) lerArquivo(Path.PRODUTOS_PATH);
+        List<Produto> lista = lerArquivoProduto();
 
         if (lista == null) {
             lista = new ArrayList<>();
@@ -78,19 +82,121 @@ public class GerenteController {
         lista.add(produto);
         escreverNoArquivo(Path.PRODUTOS_PATH, lista);
     }
-    */
 
+    // 5- Listar Produtos
+    public List<Produto> listarProdutos(){
+        return lerArquivoProduto();
+    }
 
-    // Método para ler todas as pessoas do arquivo JSON
-    public List<Gerente> lerArquivoGerente(String path) {
+    // 6- Exibir detalhes de um produto
+    public Produto exibirDetalhesProduto(int codigo){
+        Produto entidade = null;
+        List<Produto> lista = lerArquivoProduto();
+
+        if (lista == null) {
+            lista = new ArrayList<>();
+        }
+
+        for(Produto p : lista){
+            if(p.getCodigo() == codigo){
+                entidade = p;
+            }
+        }
+
+        return entidade;
+    }
+
+    // 7- Editar produto
+    public void editarProduto(int codigo, Produto atualizacao) {
+        List<Produto> produtos = lerArquivoProduto();
+
+        if (produtos != null) {
+            for (int i = 0; i < produtos.size(); i++) {
+                if (produtos.get(i).getCodigo() == codigo) {
+                    produtos.set(i, atualizacao);
+                    escreverNoArquivo(Path.PRODUTOS_PATH,produtos);
+                    System.out.println("Produto atualizado");
+                    return;
+                }
+            }
+            System.out.println("Produto não encontrado");
+        }
+    }
+
+    // 8- Registrar valor unitário de venda de produto
+
+    // 9- Cadastro de cliente
+    public void cadastrarCliente(Cliente cliente) {
+        criarArquivo(Path.CLIENTE_PATH);
+        List<Cliente> lista = lerArquivoCliente();
+
+        if (lista == null) {
+            lista = new ArrayList<>();
+        }
+
+        lista.add(cliente);
+        escreverNoArquivo(Path.CLIENTE_PATH, lista);
+    }
+
+    // 10- Enviar email com cupom de desconto
+
+    // 11- Gerar relatório de balanço mensal
+
+    // Método para ler todas as pessoas do arquivo JSON GERENTE
+    public List<Gerente> lerArquivoGerente() {
         try {
-            return objectMapper.readValue(new File(path), new TypeReference<List<Gerente>>() {
+            return objectMapper.readValue(new File(Path.GERENTE_PATH), new TypeReference<List<Gerente>>() {
             });
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
+
+    // Método para ler todas as pessoas do arquivo JSON ALMOXERIFE
+    public List<Almoxerife> lerArquivoAlmoxerife() {
+        try {
+            return objectMapper.readValue(new File(Path.ALMOXERIFE_PATH), new TypeReference<List<Almoxerife>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Método para ler todas as pessoas do arquivo JSON CAIXA ELETRÔNICO
+    public List<CaixaEletronico> lerArquivoCaixaEletronico() {
+        try {
+            return objectMapper.readValue(new File(Path.CAIXA_PATH), new TypeReference<List<CaixaEletronico>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Método para ler todas as pessoas do arquivo JSON PRODUTO
+    public List<Produto> lerArquivoProduto() {
+        try {
+            return objectMapper.readValue(new File(Path.PRODUTOS_PATH), new TypeReference<List<Produto>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Método para ler todas as pessoas do arquivo JSON CLIENTE
+    public List<Cliente> lerArquivoCliente() {
+        try {
+            return objectMapper.readValue(new File(Path.PRODUTOS_PATH), new TypeReference<List<Cliente>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
 
     // Método para escrever a lista de pessoas no arquivo JSON
